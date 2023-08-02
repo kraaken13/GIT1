c356ac58c0d1ae88e4a811f06a10b4391306549f
kraaken <vkraaken@gmail.com>
2023-08-02 11:35:51 +0300

    First class

---
+++ Main1.java
@@ -0,0 +1,24 @@
+public class Main1 {
+    public static class Dog {
+        String name;
+        int age;
+
+        void displayInfo() {
+            System.out.printf("Name: %s \tAge: %d\n", name, age);
+        }
+    }
+        public static class Cat{
+            String name;
+            int number;
+            void displayInfo() {
+                System.out.printf("This cat:%s\twho scratched that number of cupboards:%d\n",name,number);
+
+            }
+
+
+
+        }
+
+        }
+
+
